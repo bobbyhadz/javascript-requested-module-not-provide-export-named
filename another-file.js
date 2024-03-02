d817ddcdd845c0641d2833a9041910e2a381d4de
bobbyhadz <opmftw@gmail.com>
2024-03-02 11:38:54 +0200

    initial commit

---
+++ another-file.js
@@ -0,0 +1,7 @@
+// 👇️ named export
+export const num = 100;
+
+// 👇️ default export
+export default function sum(a, b) {
+  return a + b;
+}
---
+++ index.js
@@ -0,0 +1,5 @@
+// 👇️ default and named imports
+import sum, {num} from './another-file.js';
+
+console.log(sum(10, 15)); // 👉️ 25
+console.log(num); // 👉️ 100
